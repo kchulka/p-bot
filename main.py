cd3264ea2a07ef7c928470400bdb62d140fff1b7
Kchulka <kchulka008@seznam.cz>
2022-06-18 18:29:56 +0200

    kinda working bot

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 
+print(f"The bot is starting up... \n")
+
 """ ----- The import stuff ----- """
 
 import discord
 from discord import SlashCommandGroup
 from discord.ext import commands
-from discord.ui import  Button, View
+from discord.ui import Button, View
 
-#import requests
+import asyncpraw
 
 import random
+
+import re
+
+import yaml
+from yaml import Loader
+
+""" ----- settings stuff ----- """
+
+class settings:
+    def __init__(self):
+        self.file = open('resources/settings.yml', 'r')
+        self.data = yaml.load(self.file, Loader=Loader)
+        #print(self.data)
+settings = settings()
+
+
 """ ----- TOKEN ----- """
 
-TOKEN = "ODU2ODI3MTc4NzM4Mzg0ODk2.G_Xhxo.RsvGVgXn1n_70uhwonkjQujBsm17whQCv_NdqM"
+TOKEN = settings.data.get("TOKEN")
+
+""" ----- reddet ----- """
+
+reddit = asyncpraw.Reddit(
+    client_id = settings.data.get("reddit_login").get("client_id"),
+    client_secret = settings.data.get("reddit_login").get("client_secret"),
+    username = settings.data.get("reddit_login").get("username"),
+    password = settings.data.get("reddit_login").get("password"),
+    user_agent = settings.data.get("reddit_login").get("user_agent")
+)
 
 """ ----- BOT & INITIALIZATION ----- """
 
-print(f"The bot is starting up... \n")
-
 bot = discord.Bot(
-    command_prefix="ph.",
+    command_prefix="notused",
     intents=discord.Intents(members=True, messages=True, guilds=True),
-    debug_guilds=[586268358710657024]
+    debug_guilds=[]
 )
 
 @bot.event
@@ -28,127 +54,321 @@ async def on_ready():
     print(f"Bot has been logged in as {bot.user} \n"
           f"____________________________________")
 
-""" ----- NOT NSWF CHANNEL ----- """
+""" ----- embeds ----- """
 
-def notnswfchannel():
-    notnswfchannelem = discord.Embed(title="This is not a NSWF channel!",
-                                     description="I'm sorry, but this command is only allowed in NSWF channels.",
-                                     color=0xff0000
-                                     )
-    notnswfchannelem.set_thumbnail(url="https://i.pinimg.com/originals/c2/ea/1a/c2ea1a0d3e357245661a69cc83ec050a.jpg")
-    return notnswfchannelem
+class Embeds():
+
+    def notnswfchannel(slef=None):
+        embed = discord.Embed(
+            title="This is not a NSWF room!",
+            description="I'm sorry, but this command is only allowed in NSWF rooms.",
+            color=0xff0000
+            )
+        embed.set_thumbnail(url="https://i.pinimg.com/originals/c2/ea/1a/c2ea1a0d3e357245661a69cc83ec050a.jpg")
+        return embed
+
+    def fitom_klasika(slef=None):
+        embed = discord.Embed(
+            title="Enjoy the photo <3",
+            color=0x616161
+        )
+        y = random.randint(1, 4)
+        if y == 1:
+            x = random.randint(1, 100)
+        if y == 2:
+            x = random.randint(101, 200)
+        if y == 3:
+            x = random.randint(201, 300)
+        if y == 4:
+            x = random.randint(301, 400)
+        if y == 5:
+            x = random.randint(401, 500)
+        if y == 6:
+            x = random.randint(501, 600)
+        if y == 7:
+            x = random.randint(601, 700)
+        url = f'https://raw.githubusercontent.com/FitomPlays/klasika/main/{y}/{x}.jpg'
+        #print("Fitom-klasika url:", url)
+        embed.set_image(url=url)
+        return embed
+
+    def choose_category(slef=None):
+        embed = discord.Embed(
+            title="Choose category",
+            description="You can pick here between Reddit and Fitom-Klasika.",
+            color=0x616161
+            )
+        embed.set_thumbnail(url="https://www.electrokit.com/uploads/productimage/41017/ksr18_3.jpg")
+        return embed
+
+    def timeout_category(slef=None):
+        embed = discord.Embed(
+            title="Time's up!",
+            description="Please execute the command again!",
+            color=0x616161
+            )
+        embed.set_thumbnail(url="https://www.electrokit.com/uploads/productimage/41017/ksr18_3.jpg")
+        return embed
+
+    def reddit_category(self=None):
+        embed = discord.Embed(
+            title="Choose your favourite subreddit",
+            description="You can pick here between multiple choices.",
+            color=0x616161
+            )
+        embed.set_thumbnail(url="https://www.electrokit.com/uploads/productimage/41017/ksr18_3.jpg")
+        return embed
+
+    async def reddit(subreddit):
+        print(subreddit)
+        if subreddit == "all":
+            subreddit = random.choice(["nsfw", "nudes", "toocuteforporn", "pussy", "ass"])
+            print("chosen from all:", subreddit)
+        sub = await reddit.subreddit(subreddit)
+        all_subs = []
+        top = sub.hot(limit=100)
+
+        async for submission in top:
+            all_subs.append(submission)
+
+        random_sub = random.choice(all_subs)
+
+        check = r"(?:http\:|https\:)?\/\/.*\.(?:png|jpg)"
+        matches = re.search(check, random_sub.url, re.IGNORECASE)
+        print(matches)
+        while matches == None:
+            random_sub = random.choice(all_subs)
+            matches = re.search(check, random_sub.url, re.IGNORECASE)
+            print(matches)
+
+        else:
+
+            name = random_sub.title
+            embed = discord.Embed(
+                title=name,
+                color=0x616161
+            )
+
+            embed.set_image(url=random_sub.url)
+
+            return embed
 
 
-""" ----- PIC ----- """
+""" ----- CATEGORIES / Views ----- """
 
-def randompic():
-    randompic = discord.Embed(
-        title="Enjoy the photo <3",
-        color=0x616161
-                              )
-    y = random.randint(1, 2)
-    if y == 1:
-        x = random.randint(1, 100)
-    elif y == 2:
-        x = random.randint(101, 200)
-    url = f'https://raw.githubusercontent.com/FitomPlays/klasika/main/{y}/{x}.jpg'
-    print("cmd /pic", url)
-    randompic.set_image(url=url)
-    return randompic
+class View_choosecategory(View):
+    def __init__(self, ctx):
+        super().__init__(timeout=30)
+        self.ctx = ctx
+
+    @discord.ui.button(label="Reddit", style=discord.ButtonStyle.gray, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_category")
+    async def reddit_button_callback(self, button, interaction):
+        print(self.ctx)
+        em = Embeds.reddit_category()
+        vi = View_redditcategory(ctx=self.ctx)
+        await interaction.response.edit_message(content="", embed=em, view=vi)
 
 
-class randompicView(View):
-    def __init__(self):
-        super().__init__(timeout=2)
+    @discord.ui.button(label="Fitom-Klasika", style=discord.ButtonStyle.gray, emoji="<:fitom:987691880085073981>", disabled=False, custom_id="fitom_category")
+    async def fitom_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = Embeds.fitom_klasika()
+        vi = View_fitomklasika(ctx=self.ctx)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+    async def on_timeout(self):
+        #print("timeout")
+        try:
+            em = Embeds.timeout_category()
+            redditbutton = [x for x in self.children if x.custom_id == "reddit_category"][0]
+            redditbutton.disabled = True
+            fitombutton = [x for x in self.children if x.custom_id == "fitom_category"][0]
+            fitombutton.disabled = True
+            # print(self.ctx)
+            await self.ctx.edit(content="", embed=em, view=self)
+        except:
+            print("message was deleted")
 
 
-    @discord.ui.button(label="regenerate", style=discord.ButtonStyle.blurple, emoji="🥰", disabled=False, custom_id="regen_pick")
+class View_redditcategory(View):
+    def __init__(self, ctx):
+        super().__init__(timeout=30)
+        self.ctx = ctx
+
+
+    @discord.ui.button(label="All", style=discord.ButtonStyle.red, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_c_all")
+    async def all_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+        sub="all"
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+
+    @discord.ui.button(label="NSFW", style=discord.ButtonStyle.red, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_c_nsfw")
+    async def nsfw_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+        sub="nsfw"
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+
+    @discord.ui.button(label="Nudes", style=discord.ButtonStyle.red, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_c_Nudes")
+    async def nudes_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+        sub="nudes"
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+    @discord.ui.button(label="Pussy", style=discord.ButtonStyle.red, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_c_pussy")
+    async def pussy_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+        sub="pussy"
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+    @discord.ui.button(label="Ass", style=discord.ButtonStyle.red, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_c_ass")
+    async def ass_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+        sub="ass"
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+    @discord.ui.button(label="Too cute for porn", style=discord.ButtonStyle.red, emoji="<:reddit:987690510481231942>", disabled=False, custom_id="reddit_c_tcfp")
+    async def tcfp_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=" ", delete_after=0.1)
+        sub="toocuteforporn"
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+class View_reddit(View):
+    def __init__(self, ctx, subreddit):
+        super().__init__(timeout=30)
+        self.ctx = ctx
+        self.sub = subreddit
+
+
+    @discord.ui.button(label="regenerate", style=discord.ButtonStyle.blurple, emoji="🥰", disabled=False, custom_id="reddit_regen")
     async def regen_button_callback(self, button, interaction):
-        print(button)
-        button.label = "test"
-        print(button)
-        em = randompic()
-        vi = randompicView()
+        await interaction.response.edit_message(content="Loading...")
+        em = await Embeds.reddit(subreddit=self.sub)
+        await self.ctx.edit(content="", embed=em)
+
+
+    @discord.ui.button(label="new picture", style=discord.ButtonStyle.red, emoji="😈", disabled=False, custom_id="reddit_new")
+    async def new_button_callback(self, button, interaction):
+        button.disabled = True
+        regenbutton = [x for x in self.children if x.custom_id=="reddit_regen"][0]
+        regenbutton.disabled = True
+        await interaction.response.edit_message(content="", view=self)
+
+
+        self.ctx = await self.ctx.channel.send(content="loading...")
+        em = await Embeds.reddit(subreddit=self.sub)
+        vi = View_reddit(ctx=self.ctx, subreddit=self.sub)
+        await self.ctx.edit(content="", embed=em, view=vi)
+
+    async def on_timeout(self):
+        try:
+            #print("timeout")
+            regenbutton = [x for x in self.children if x.custom_id == "reddit_regen"][0]
+            regenbutton.disabled = True
+            newbutton = [x for x in self.children if x.custom_id == "reddit_new"][0]
+            newbutton.disabled = True
+            #print(self.ctx)
+            await self.ctx.edit(content="", view=self)
+        except:
+            print("message was deleted")
+
+class View_fitomklasika(View):
+    def __init__(self, ctx):
+        super().__init__(timeout=30)
+        self.ctx = ctx
+
+    @discord.ui.button(label="regenerate", style=discord.ButtonStyle.blurple, emoji="🥰", disabled=False, custom_id="regen_pic")
+    async def regen_button_callback(self, button, interaction):
+        em = Embeds.fitom_klasika()
+        vi = View_fitomklasika(ctx=self.ctx)
 
         await interaction.response.edit_message(embed=em, view=vi)
 
 
-    @discord.ui.button(label="new picture", style=discord.ButtonStyle.red, emoji="😈", disabled=False, custom_id="new_pick")
+    @discord.ui.button(label="new picture", style=discord.ButtonStyle.red, emoji="😈", disabled=False, custom_id="new_pic")
     async def new_button_callback(self, button, interaction):
-        print(button)
         button.disabled = True
-        print(button)
-        regenbutton = [x for x in self.children if x.custom_id=="regen_pick"][0]
+        regenbutton = [x for x in self.children if x.custom_id=="regen_pic"][0]
         regenbutton.disabled = True
         await interaction.response.edit_message(content="", view=self)
-        em = randompic()
-        vi = randompicView()
-        await interaction.followup.send(embed=em, view=vi)
 
+        em = Embeds.fitom_klasika()
+        vi = View_fitomklasika(ctx=self.ctx)
+        self.ctx = await interaction.followup.send(embed=em, view=vi)
 
     async def on_timeout(self):
-        print("timeout")
-        regenbutton = [x for x in self.children if x.custom_id=="regen_pick"][0]
-        regenbutton.disabled = True
-        await self.message.edit(content="", view=self)
+        try:
+            #print("timeout")
+            regenbutton = [x for x in self.children if x.custom_id == "regen_pic"][0]
+            regenbutton.disabled = True
+            newbutton = [x for x in self.children if x.custom_id == "new_pic"][0]
+            newbutton.disabled = True
+            #print(self.ctx)
+            await self.ctx.edit(content="", view=self)
+        except:
+            print("message was deleted")
 
+""" ----- COMMANDS ----- """
 
 @bot.command(
-    description="Get a random pic!"
+    name="pic-random",
+    description="Get a random pic from all categories!"
 )
-async def pic(ctx):
+async def picrandom(ctx):
     if ctx.channel.is_nsfw() == False:
-        em = notnswfchannel()
-        await ctx.respond(embed=em)
+        em = Embeds.notnswfchannel()
+        await ctx.respond(embed=em, delete_after=10)
     else:
-        em = randompic()
-        vi = randompicView()
+        em = Embeds.fitom_klasika()
+        vi = View_fitomklasika(ctx)
+        print(ctx)
         await ctx.respond(embed=em, view=vi)
 
-
-
-
-
-
-
-
-
-
-""" ----- The Picture stuff ----- """
-
-picture = SlashCommandGroup(name="picture", description="picture stuff.")
-category = picture.create_subgroup("category", "test")
-
-
-
-@category.command(
-    name="test",
-    description="test.",
-    guild_ids=[586268358710657024]
+@bot.command(
+    name="pic-category",
+    description="Get a random pic from a category chosen by you!"
 )
-async def test(ctx):
-    await ctx.respond("hm")
+async def piccategory(ctx):
+    if ctx.channel.is_nsfw() == False:
+        em = Embeds.notnswfchannel()
+        await ctx.respond(embed=em, delete_after=10)
 
-bot.add_application_command(picture)
+    else:
+        vi = View_choosecategory(ctx)
+        em = Embeds.choose_category()
 
-""" ----- The downloader stuff ----- """
+        await ctx.respond(content="", embed=em, view=vi)
 
-downloader = SlashCommandGroup(name="downloader", description="Used for downloading stuff from PH.")
-
-#class View(discord.ui.View): # Create a class called View that subclasses discord.ui.View
-#    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.blurple, emoji="😎") # Create a button with the label "😎 Click me!" with color Blurple
-#    async def button_callback(self, button, interaction):
-#        await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked
-
-@downloader.command(
-    name="phdwnld",
-    description="Donwload videos prom pornhub.",
-    guild_ids=[586268358710657024])
-async def phdwnld(ctx):
-    await ctx.respond("This feature is in progress!", view=View())
-
-#bot.add_application_command(downloader)
-
+@bot.command(
+    name="help",
+    description="Get some help!"
+)
+async def help(ctx):
+    em = Embeds.notnswfchannel()
+    vi = View_fitomklasika(ctx)
+    print(ctx)
+    await ctx.respond(content="help")
 
 """ ----- run ----- """
 
